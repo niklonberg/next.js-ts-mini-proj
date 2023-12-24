079d32cf4ea8c907c1aca2b1cbecf5f374723684
Nikolai Lønberg <niklonberg@gmail.com>
2023-12-24 13:39:05 +0100

    add interface PostCardProps

--- ninja-blog/src/app/page.tsx
+++ ninja-blog/src/app/page.tsx
@@ -1,9 +1,11 @@
+import PostCard from "@/components/PostCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <h2>Home</h2>
+      <PostCard title="A great title" author="Mario" />
     </main>
   );
 }
--- ninja-blog/src/components/PostCard.tsx
+++ ninja-blog/src/components/PostCard.tsx
@@ -1,10 +1,9 @@
-export default function PostCard({
-  title,
-  author,
-}: {
+interface PostCardProps {
   title: string;
   author: string;
-}) {
+}
+
+export default function PostCard({ title, author }: PostCardProps) {
   return (
     <div className="card">
       <h2>{title}</h2>
