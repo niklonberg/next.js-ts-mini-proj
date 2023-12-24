a1083513fecd39e4bfc05c080377f1bb65ad1622
Nikolai Lønberg <niklonberg@gmail.com>
2023-12-24 13:44:32 +0100

    add fetchPosts method, use it in Home fn

--- ninja-blog/src/app/page.tsx
+++ ninja-blog/src/app/page.tsx
@@ -1,7 +1,21 @@
 import PostCard from "@/components/PostCard";
 import Image from "next/image";
 
-export default function Home() {
+const fetchPosts = async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/users/1/posts"
+  );
+
+  if (!response.ok) {
+    console.log("couldnt fetch data");
+  }
+
+  return response.json();
+};
+
+export default async function Home() {
+  const posts = await fetchPosts();
+
   return (
     <main>
       <h2>Home</h2>
