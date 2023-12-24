e2830c01ca7f207967cf1b1f0da55bc97ce814ad
Nikolai Lønberg <niklonberg@gmail.com>
2023-12-24 13:21:30 +0100

    add about page, reset globals.css

---
+++ ninja-blog/src/app/about/page.tsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia quo ea
+        esse, voluptatem accusantium error voluptates numquam porro rerum
+        commodi, maiores nemo dolores nostrum, odit ipsa non est velit.
+        Consequatur odio, provident officia obcaecati voluptate quaerat mollitia
+        unde optio nulla voluptatibus suscipit veritatis atque fuga laborum
+        accusamus perspiciatis nihil sunt!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        maiores tempore mollitia fuga officia eos, aliquid quaerat quia animi
+        impedit neque delectus consequuntur? Ex ipsam, corrupti quas qui
+        sapiente, impedit aliquid maxime aut eaque aperiam a praesentium natus
+        vero possimus quia. Perferendis itaque ipsa distinctio dignissimos
+        exercitationem deserunt ea expedita asperiores maiores possimus officia
+        unde, assumenda voluptatem excepturi! Odio reiciendis fugiat eaque
+        deserunt aperiam id esse necessitatibus, asperiores aspernatur error
+        placeat velit voluptatum, voluptas veritatis!
+      </p>
+    </main>
+  );
+}
