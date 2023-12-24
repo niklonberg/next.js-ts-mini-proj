85e3d9e5d8a2ccc816dbf0eb4e27d919e12e8e3d
Nikolai Lønberg <niklonberg@gmail.com>
2023-12-24 13:36:15 +0100

    add PostCard component

---
+++ ninja-blog/src/components/PostCard.tsx
@@ -0,0 +1,14 @@
+export default function PostCard({
+  title,
+  author,
+}: {
+  title: string;
+  author: string;
+}) {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <p>Written by {author}</p>
+    </div>
+  );
+}
